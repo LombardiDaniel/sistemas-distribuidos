caab97d8964ee2e87b9359f3829534882f89a583
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2024-06-08 16:02:28 -0300

    kafka

--- 2/kafka/consumer.py
+++ 2/kafka/consumer.py
@@ -1,4 +1,5 @@
 import json
+from time import sleep
 
 from confluent_kafka import OFFSET_BEGINNING, Consumer
 
@@ -17,27 +18,28 @@ TOPIC = "credit-card-transactions"
 def process_transaction(transaction):
     """Process the credit card transaction."""
     print(f"Processing transaction: {transaction}")
+    sleep(1)
 
 
-# def kafka_on_assign_callback_rollback(consumer, topic_partitions):
-#     for topic_partition in topic_partitions:
-#         min_offset, max_offset = consumer.get_watermark_offsets(
-#             topic_partition, cached=False
-#         )
-#         if max_offset <= 0:
-#             topic_partition.offset = OFFSET_BEGINNING
+def kafka_on_assign_callback_rollback(consumer, topic_partitions):
+    for topic_partition in topic_partitions:
+        min_offset, max_offset = consumer.get_watermark_offsets(
+            topic_partition, cached=False
+        )
+        if max_offset <= 0:
+            topic_partition.offset = OFFSET_BEGINNING
 
-#         else:
-#             desired_offset = max_offset - KAFKA_MAX_HISTORY_DEPTH_ROLLBACK
-#             if desired_offset <= min_offset:
-#                 desired_offset = OFFSET_BEGINNING
-#             topic_partition.offset = desired_offset
-#             consumer.assign([topic_partition])
+        else:
+            desired_offset = max_offset - KAFKA_MAX_HISTORY_DEPTH_ROLLBACK
+            if desired_offset <= min_offset:
+                desired_offset = OFFSET_BEGINNING
+            topic_partition.offset = desired_offset
+            consumer.assign([topic_partition])
 
 
 def main():
-    # CONSUMER.subscribe([TOPIC], on_assign=kafka_on_assign_callback_rollback)
-    CONSUMER.subscribe([TOPIC])
+    CONSUMER.subscribe([TOPIC], on_assign=kafka_on_assign_callback_rollback)
+    # CONSUMER.subscribe([TOPIC])
 
     while True:
         msg = CONSUMER.poll(1.0)  # Wait for a message up to 1 second
